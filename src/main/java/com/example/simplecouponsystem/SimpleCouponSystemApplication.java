73448f6f24a5009a21ba62ed6340aa634b62b7ec
daily1313 <kimsb7218@naver.com>
2024-03-10 21:11:20 +0900

    Initial commit

---
+++ src/main/java/com/example/simplecouponsystem/SimpleCouponSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplecouponsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCouponSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCouponSystemApplication.class, args);
+    }
+
+}
