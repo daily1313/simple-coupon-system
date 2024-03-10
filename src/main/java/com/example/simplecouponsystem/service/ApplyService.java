2acb73fd532d7e1c8cd9cb95e746f462fa623d3a
daily1313 <kimsb7218@naver.com>
2024-03-10 22:04:56 +0900

    feat: Apply redis to resolve race condition

--- src/main/java/com/example/simplecouponsystem/service/ApplyService.java
+++ src/main/java/com/example/simplecouponsystem/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.simplecouponsystem.service;
 
 import com.example.simplecouponsystem.domain.Coupon;
+import com.example.simplecouponsystem.repository.CouponCountRepository;
 import com.example.simplecouponsystem.repository.CouponRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,13 +9,16 @@ import org.springframework.stereotype.Service;
 public class ApplyService {
 
     private final CouponRepository couponRepository;
+    private final CouponCountRepository couponCountRepository;
 
-    public ApplyService(final CouponRepository couponRepository) {
+    public ApplyService(final CouponRepository couponRepository,
+                        final CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
     }
 
     public void apply(final Long userId) {
-        long count = couponRepository.count();
+        long count = couponCountRepository.increment();
 
         if (count > 100) {
             return;
@@ -22,4 +26,6 @@ public class ApplyService {
 
         couponRepository.save(new Coupon(userId));
     }
+
+
 }
