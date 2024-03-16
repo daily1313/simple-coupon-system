626a392446ff5a74fcb3a8bba367096789778d6c
daily1313 <kimsb7218@naver.com>
2024-03-16 16:46:33 +0900

    feat: Apply redis to limit the number of coupon issuance to one per person

---
+++ src/main/java/com/example/simplecouponsystem/repository/AppliedUserRepository.java
@@ -0,0 +1,20 @@
+package com.example.simplecouponsystem.repository;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AppliedUserRepository {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public AppliedUserRepository(final RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public Long add(final Long userId) {
+        return redisTemplate
+                .opsForSet()
+                .add("applied_user", userId.toString());
+    }
+}
--- src/main/java/com/example/simplecouponsystem/service/ApplyService.java
+++ src/main/java/com/example/simplecouponsystem/service/ApplyService.java
@@ -2,6 +2,7 @@ package com.example.simplecouponsystem.service;
 
 import com.example.simplecouponsystem.domain.Coupon;
 import com.example.simplecouponsystem.producer.CouponCreateProducer;
+import com.example.simplecouponsystem.repository.AppliedUserRepository;
 import com.example.simplecouponsystem.repository.CouponCountRepository;
 import com.example.simplecouponsystem.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -12,16 +13,25 @@ public class ApplyService {
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
     private final CouponCreateProducer couponCreateProducer;
+    private final AppliedUserRepository appliedUserRepository;
 
     public ApplyService(final CouponRepository couponRepository,
                         final CouponCountRepository couponCountRepository,
-                        final CouponCreateProducer couponCreateProducer) {
+                        final CouponCreateProducer couponCreateProducer,
+                        final AppliedUserRepository appliedUserRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(final Long userId) {
+        Long apply = appliedUserRepository.add(userId);
+
+        if (apply != 1) {
+            return;
+        }
+
         long count = couponCountRepository.increment();
 
         if (count > 100) {
